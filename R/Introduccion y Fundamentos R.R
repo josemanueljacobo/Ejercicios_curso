7e052359c34fd6a1a6ceac13d5866a87534d8e75
josemanueljacobo <josemanueljacobo85@gmail.com>
2025-03-30 20:43:48 +0200

    actualizacion R

--- R/Introduccion y Fundamentos R.R
+++ R/Introduccion y Fundamentos R.R
@@ -1,13 +1,15 @@
 # Ejercicio 1: Variables y Tipos de Datos
 nombre <- "Jose Manuel"
+numero <- 10
 
 # Ejercicio 2: Funciones class e is.numeric
-numero <- 10
+
 tipo_numero <- class(numero)
 es_numerico <- is.numeric(numero)
 
 # Ejercicio 3: Operaciones Aritméticas
 suma_numero <- numero + 5  
+doble_numero <- numero * 2
 
 # Ejercicio 4: Vectores y Listas
 edades <- c(25, 30, 18)  
@@ -16,25 +18,25 @@ informacion <- list(nombre = "Juan", edad = 25)
 
 # Ejercicio 5: Funciones is.character e is.logical
 es_caracter <- is.character(nombre)
-es_logico <- is.logical(TRUE)  # Ejemplo con un valor lógico
+es_logico <- is.logical(TRUE) 
 
 # Ejercicio 6: Operaciones Lógicas
-mayor_de_edad <- edades[1] >= 18  # Verifica si la primera edad es mayor o igual a 18
+mayor_de_edad <- edades[1] >= 18  
 
 # Ejercicio 7: Comparaciones de Vectores
-esta_30_en_edades <- 30 %in% edades  # Verifica si 30 está en edades
+esta_30_en_edades <- 30 %in% edades 
 
 # Ejercicio 8: Operadores de Comparación
-doble_numero_mayor_edad3 <- (numero * 2) > edades[3]  # Compara si el doble de numero es mayor que edades[3]
+doble_numero_mayor_edad3 <- (numero * 2) > edades[3] 
 
 # Ejercicio 9: Utilizar Operador Lógico
 condicion1 <- TRUE
 condicion2 <- TRUE
-ambas_verdaderas <- condicion1 & condicion2  # Comprueba si ambas son TRUE
+ambas_verdaderas <- condicion1 & condicion2 
 
 # Ejercicio 10: Utilizar Operador Lógico con Negación
 verdadero <- TRUE
-negacion_verdadero <- !verdadero  # Comprueba que su valor NO sea verdadero
+negacion_verdadero <- !verdadero 
 
 # Imprimir resultados para verificación
 print(list(
